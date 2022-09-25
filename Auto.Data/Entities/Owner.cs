ab1194dd20f3c7b53abed152d362ad6ba89fcf34
AntonovAndrei <camuy4onkoa4a@yandex.ru>
2022-09-25 16:56:42 +0300

    Добавьте файлы проекта.

---
+++ Auto.Data/Entities/Owner.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace Auto.Data.Entities
+{
+    public class Owner
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string VehicleId { get; set; }
+
+        [JsonIgnore]
+        public virtual Vehicle Vehicle { get; set; }
+    }
+}
